71e05487dbf10c39bbfedb13d00bee4cef5350b4
Piotr Usewicz <piotr@layer22.com>
2023-01-31 19:41:00 +0100

    Add cert command

--- lib/ruby_tak/cli.rb
+++ lib/ruby_tak/cli.rb
@@ -18,6 +18,14 @@ module RubyTAK
         s.action { |_args, _options| RubyTAK::Server.start }
       end
 
+      command :certificate do |c|
+        c.syntax = "ruby_tak certificate [options]"
+        c.summary = "Manage certificates"
+        c.description = "Allows for creating certificate authority and client certificates"
+        c.example "description", "command example"
+        c.when_called RubyTAK::CLI::Commands::Certificate
+      end
+
       run!
     end
   end
