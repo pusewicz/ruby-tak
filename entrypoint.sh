c42461d0a006ac1d5d5a0f5bc1e152c032cbcd0a
Piotr Usewicz <piotr@layer22.com>
2023-10-01 19:35:33 +0200

    Remove --trace

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 if [ -z "$1" ]; then
   echo "Starting RubyTak server..."
-  exec ./exe/ruby_tak server --trace
+  exec ./exe/ruby_tak server
 else
   echo "Running RubyTak command... $@"
   exec ./exe/ruby_tak "$@"
