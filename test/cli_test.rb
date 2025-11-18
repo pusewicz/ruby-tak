b9c22a54226f92e0e6d9724e67faf32f96570954
Piotr Usewicz <piotr@layer22.com>
2025-11-18 12:59:55 +0100

    Add certificate generation

--- test/cli_test.rb
+++ test/cli_test.rb
@@ -40,4 +40,84 @@ class CLITest < Minitest::Test
       assert_equal 8089, RubyTAK.configuration.cot_ssl_port
     end
   end
+
+  def test_certificate_ca_generates_files
+    Dir.mktmpdir do |tmpdir|
+      config = RubyTAK.configuration
+      config.stub :certs_dir, Pathname.new(tmpdir) do
+        ca_key_path = Pathname.new(tmpdir).join(config.ca_key)
+        ca_crt_path = Pathname.new(tmpdir).join(config.ca_crt)
+
+        capture_io do
+          @cli.run(%w[certificate ca])
+        end
+
+        assert_predicate ca_key_path, :exist?, "CA key file should exist"
+        assert_predicate ca_crt_path, :exist?, "CA certificate file should exist"
+
+        # Verify key permissions
+        assert_equal 0o100600, ca_key_path.stat.mode
+      end
+    end
+  end
+
+  def test_certificate_ca_generates_valid_x509
+    Dir.mktmpdir do |tmpdir|
+      config = RubyTAK.configuration
+      config.stub :certs_dir, Pathname.new(tmpdir) do
+        ca_crt_path = Pathname.new(tmpdir).join(config.ca_crt)
+
+        capture_io do
+          @cli.run(%w[certificate ca])
+        end
+
+        # Verify certificate is valid X.509
+        cert = OpenSSL::X509::Certificate.new(File.read(ca_crt_path))
+
+        assert_equal "RubyTAK CA", cert.subject.to_a.find { |attr| attr[0] == "CN" }[1]
+      end
+    end
+  end
+
+  def test_certificate_ca_skips_if_exists
+    Dir.mktmpdir do |tmpdir|
+      config = RubyTAK.configuration
+      config.stub :certs_dir, Pathname.new(tmpdir) do
+        # Generate once
+        capture_io { @cli.run(%w[certificate ca]) }
+
+        # Try to generate again
+        output = capture_io do
+          @cli.run(%w[certificate ca])
+        end
+
+        assert_match(/already exists/, output[0])
+        assert_match(/Skipping generation/, output[0])
+      end
+    end
+  end
+
+  def test_certificate_invalid_type_shows_error
+    error = nil
+    output = capture_io do
+      error = assert_raises(SystemExit) do
+        @cli.run(%w[certificate invalid])
+      end
+    end
+
+    assert_equal 1, error.status
+    assert_match(/Unknown certificate type 'invalid'/, output[0])
+  end
+
+  def test_certificate_server_not_implemented
+    error = nil
+    output = capture_io do
+      error = assert_raises(SystemExit) do
+        @cli.run(%w[certificate server])
+      end
+    end
+
+    assert_equal 1, error.status
+    assert_match(/not yet implemented/, output[0])
+  end
 end
